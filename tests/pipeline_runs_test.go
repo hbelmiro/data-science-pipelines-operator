288187cec0cf14b9059dd62f1bc673d34bfffd3d
hbelmiro <helber.belmiro@gmail.com>
2024-06-12 11:42:35 -0300

    Fixed flaky test IntegrationTestSuite#TestPipelineSuccessfulRun
    Signed-off-by: hbelmiro <helber.belmiro@gmail.com>

--- tests/pipeline_runs_test.go
+++ tests/pipeline_runs_test.go
@@ -35,7 +35,8 @@ func (suite *IntegrationTestSuite) TestPipelineSuccessfulRun() {
 	suite.T().Run("Should create a Pipeline Run", func(t *testing.T) {
 		// Retrieve Pipeline ID to create a new run
 		pipelineDisplayName := "[Demo] iris-training"
-		pipelineID := TestUtil.RetrievePipelineId(t, APIServerURL, pipelineDisplayName)
+		pipelineID, err := TestUtil.RetrievePipelineId(t, APIServerURL, pipelineDisplayName)
+		require.NoError(t, err)
 		postUrl := fmt.Sprintf("%s/apis/v2beta1/runs", APIServerURL)
 		body := TestUtil.FormatRequestBody(t, pipelineID, pipelineDisplayName)
 		contentType := "application/json"
@@ -55,7 +56,8 @@ func (suite *IntegrationTestSuite) TestPipelineSuccessfulRun() {
 	suite.T().Run("Should create a Pipeline Run using custom pip server", func(t *testing.T) {
 		// Retrieve Pipeline ID to create a new run
 		pipelineDisplayName := "Test pipeline run with custom pip server"
-		pipelineID := TestUtil.RetrievePipelineId(t, APIServerURL, pipelineDisplayName)
+		pipelineID, err := TestUtil.RetrievePipelineId(t, APIServerURL, pipelineDisplayName)
+		require.NoError(t, err)
 		postUrl := fmt.Sprintf("%s/apis/v2beta1/runs", APIServerURL)
 		body := TestUtil.FormatRequestBody(t, pipelineID, pipelineDisplayName)
 		contentType := "application/json"
